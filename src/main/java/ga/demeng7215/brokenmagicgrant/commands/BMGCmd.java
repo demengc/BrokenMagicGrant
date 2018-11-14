89e3d163152e4f91ffec46bb05b2ddfc8777ec4e
Demeng <Demeng7215@users.noreply.github.com>
2018-11-14 18:42:01 -0500

    Initial commit

--- src/main/java/ga/demeng7215/brokenmagicgrant/commands/BMGCmd.java
+++ src/main/java/ga/demeng7215/brokenmagicgrant/commands/BMGCmd.java
@@ -11,7 +11,7 @@ public class BMGCmd implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        Bukkit.getConsoleSender().sendMessage(BrokenMagicGrant.format("&9Running BrokenMagicGrant v1.0.0 by Demeng7215"));
+        sender.sendMessage(BrokenMagicGrant.format("&9Running BrokenMagicGrant v1.0.0 by Demeng7215"));
 
         return false;
     }
